f28b508855bae7cbc08b032eeddc6dc80fc79d08
Manjunath RS <manjunath.rs@unilogcorp.com>
2020-05-19 10:25:41 +0530

    guestcheckout

--- WEB_THEMES.war/ECOMMERCE_STD_TEMPLATE_V2/js/CartPage.js
+++ WEB_THEMES.war/ECOMMERCE_STD_TEMPLATE_V2/js/CartPage.js
@@ -9,14 +9,6 @@ $(document).ready(function(){
 		"sDom": 't<"row"<"col-md-6 col-ms-6 col-sm-12 cartPagination"p><"col-md-6 col-ms-6 col-sm-12 cartTotalBlock">>'
 	});
 	$('.cartTotalBlock').html($("#copyPrice").html());
-	jQuery.ajax({ 
-		type: "POST",
-		url: "/SalesPromotionSynchService.slt?loadKieModule=true",
-		data:"",
-		success: function(msg){
-			console.log(msg);
-		}
-	});
 });
 $('[data-function="saveCartFunction"]').click(function() {
 	var toggleListID = "#"+$(this).attr('data-listTarget');
@@ -65,7 +57,7 @@ function submitSaveCart(title,groupId,obj,isReorder){
 		}
 	}
 	jQuery.get('saveCartPage.action?listId='+groupId+'&listName='+title+'&isReOrder='+isReorder,function(data,status){
-		$(toggleListID).hide();
+		$(toggleListID).toggle();
 		var result = data.split('|');
 		//$(toggleListID+"_custPop").html("Cart Saved Successfully - "+ $("#group_name").val()).attr("href","myProductGroupPage.action?savedGroupId="+result[1]).fadeIn();
 		$(toggleListID+"_custPop").html("Cart Saved Successfully - "+ groupName).attr("href","/"+result[1]+"/ProductGroup/Cart?savedGroupName="+groupName).fadeIn();
@@ -179,10 +171,9 @@ function processAction(r,path,typ,item){
 	}
 }
 function refreshShoppingCart(id,partNum){
-	var originalPartNumber = partNum;
-	//partNum = partNum.replace(/ +/g,"_");
+	partNum = partNum.replace(/ +/g,"_");
 	partNum = partNum.replace(/[#;&,.+*~':"!^$[\]()=>|\/ ]/g, "\\$&");
-	var curQty = $("#textQty_"+partNum).val();
+	var curQty = $("#textQtyCur_"+partNum).val();
 	var mpnDisplay = "";
 	var mpn="";
 	var valu ="";
@@ -190,14 +181,12 @@ function refreshShoppingCart(id,partNum){
 	if(id != ""){
 		$("#refreshCartId").val(id);
 		$("#refreshQty").val($("#textQty_"+partNum).val());
-		//$("#refreshQty").val($.trim($("[data-cartitemid='"+id+"']").val()));
 		$("#lineItemCommentRef").val($("#lineItemComment_"+id).val());
 		if($("#requiredByDateRef").length > 0){
 			$("#requiredByDateRef").val($("#reqDate_"+id).val());
 		}
 	}
 	var qtyEntered = parseInt($.trim($("#textQty_"+partNum).val()));
-	//var qtyEntered = parseInt($.trim($("[data-cartitemid='"+id+"']").val()));
 	if($("#mpn_"+partNum).length > 0 && $("#mpnDisplay"+partNum).length > 0){
 		mpn = $("#mpn_"+partNum).val();
 		mpnDisplay = $("#mpnDisplay"+partNum).val();
@@ -239,9 +228,9 @@ function refreshShoppingCart(id,partNum){
 				}
 			}else{
 				if(lessThanMinOrder){
-					valu = "Item# "+originalPartNumber+" is only available in multiples of "+orderQtyInterval+" and Min. Order Quantity is : "+minimumOrderQty;
+					valu = "Item# "+partNum+" is only available in multiples of "+orderQtyInterval+" and Min. Order Quantity is : "+minimumOrderQty;
 				}else{
-					valu = "Item# "+originalPartNumber+" is only available in multiples of "+orderQtyInterval;
+					valu = "Item# "+partNum+" is only available in multiples of "+orderQtyInterval;
 				}
 			}
 			showNotificationDiv("error", valu);
@@ -370,7 +359,8 @@ function standardCheckout(){
 							if(typeof userLogin!="undefined" && userLogin=="true"){
 								$("#updateCartForm").attr("action","checkout.action");
 							}else{
-								$("#updateCartForm").attr("action","Login");
+								//$("#updateCartForm").attr("action","Login");
+								$("#updateCartForm").attr("action","guestcheckout.action");
 							}
 						}
 						$("#updateCartForm").submit();
@@ -528,7 +518,6 @@ function sendApproval() {
 		this.qtyInterval = attributes.qtyinterval;
 		this.uom = attributes.uom;
 		this.itemPriceId = attributes.itempriceid;
-		this.value = attributes.value;
 	}
 	
 	function extractItemDetails(element){
@@ -623,7 +612,7 @@ function sendApproval() {
 				persistItem(myCart.storeName, item);
 			}else{
 				if(element.type == "TEXT" || element.type == "text"){
-					element.value = item.value != 0 ? item.value : item.minOrderQty;
+					element.value = item.minOrderQty;
 				}
 				bootAlert("medium","warning","warning",status.description);
 			}
@@ -846,63 +835,3 @@ function sendApproval() {
 	
 	setItemsToLocalStorage(myCart.storeName, []);
 })();
-function editevent(partNum){
-	var itempartnumber=partNum
-	$('.editpricesales_'+itempartnumber).css('display','block');
-    $('#editunitprice_'+itempartnumber).css('display' , 'none');
-	
-}
-function editPrice(productListId){
-	$('.editpricesales_'+productListId).removeClass('hideMe');
-	$('#editunitprice_'+productListId).addClass('hideMe');	
-}
-
-function cancelUpdatePrice(productListId){
-    $('#editunitprice_'+productListId).removeClass('hideMe');
-    $('.editpricesales_'+productListId).addClass('hideMe');
-}
-
-function updatePrice(productListId, partNum){
-	var unitPrice = $('#unitPrice_'+productListId).val();
-	var updatedUnitPrice = $('#updatedUnitPrice_'+productListId).val();
-	var uom = $('#uom_'+productListId).val();
-	var getPriceFrom = 'SALESREP';
-	var itemQty=$("#textQty_"+partNum).val();
-	if(updatedUnitPrice <= 0){
-		bootAlert("small","error","Error","Cannot update item with zero or less than zero price.");
-	}else if(unitPrice != updatedUnitPrice){
-		var cartId = productListId;
-		if(productListId != ""){
-			$("#refreshCartId").val(productListId);
-			if(typeof cartId!="undefined" && cartId!=null && cartId!=""){
-				$("#refreshQty").val($(".textQty_"+cartId).val());
-			}
-			else{$("#refreshQty").val($("#textQty_"+partNum).val());}
-			
-			$("#lineItemCommentRef").val($("#lineItemComment_"+productListId).val());
-			if($("#requiredByDateRef").length > 0){
-				$("#requiredByDateRef").val($("#reqDate_"+productListId).val());
-			}
-		}
-		
-		var str = "&updateId="+productListId+"&unitPrice="+unitPrice+"&updatedUnitPrice="+updatedUnitPrice+"&uom="+uom+"&getPriceFrom="+getPriceFrom+"&itemQty="+itemQty;
-		block("Please Wait");
-		$.ajax({
-			type: "POST",
-			url:"updateCustomerPricePage.action",
-			data: str,
-			success: function(response){
-				unblock();
-				if(response){
-					bootAlert("small","success","Success","Cart Updated");
-					window.location.href = "shoppingCartPage.action";
-				}else{
-					bootAlert("small","error","Error","Cart Rejected Failed. Please try again");
-				}
-			}
-		});
-	}else{
-		unblock();
-		cancelUpdatePrice(productListId);
-	}
-}
